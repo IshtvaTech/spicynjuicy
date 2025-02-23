07eca56ab183e27b285fab208432de3e89d86822
IshtvaTech <techdeveloperishtva@gmail.com>
2025-02-23 23:55:30 +0530

    updated logos

--- client/src/components/ProductBenefits.jsx
+++ client/src/components/ProductBenefits.jsx
@@ -3,22 +3,20 @@ import { assets } from "../assets/assets.js";
 
 const ProductBenefits = () => {
   const benefits = [
-    { img: assets.protine, text: "10-12G OF PROTEIN" },
-    { img: assets.suger, text: "0G SUGAR" },
-    { img: assets.keto, text: "KETO FRIENDLY" },
-    { img: assets.carb, text: "LOW-CARB" },
-    { img: assets.allergy, text: "ALLERGY FRIENDLY" },
-    { img: assets.filler, text: "NO FILLERS" },
+    { img: assets.memories, text: "LIVE MEMORIES" },
+    { img: assets.noSugar, text: "NO SUGAR" },
+    { img: assets.Natural, text: "NATURALLY JUICY" },
+    { img: assets.Ice, text: "Mineral Water" },
+    { img: assets.EcoFriedly, text: "Eco Friendly" },
+    { img: assets.filler, text: "NO ARTIFICIALS" },
   ];
 
   return (
     <div className="text-center py-10 mt-10">
-      
       <h2 className="text-[80px] font-bold uppercase tracking-wide">
         Cool, Refreshing, and Bursting <br /> with Flavor!
       </h2>
 
-      
       <div className="flex flex-wrap justify-center gap-10 mt-10">
         {benefits.map((item, index) => (
           <div key={index} className="flex flex-col items-center w-48">
