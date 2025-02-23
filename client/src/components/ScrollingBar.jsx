b0b080c791ea7f710dc5d00e91840366e5d44d69
IshtvaTech <techdeveloperishtva@gmail.com>
2025-02-24 00:27:44 +0530

    updated marq

--- client/src/components/ScrollingBar.jsx
+++ client/src/components/ScrollingBar.jsx
@@ -5,18 +5,51 @@ const ScrollingBar = () => {
     <div className="bg-red-600 py-3 overflow-hidden whitespace-nowrap  mt-30 h-[100px] flex items-center justify-center">
       <div className="flex w-max animate-marquee items-center">
         <div className="flex text-white text-2xl font-bold uppercase gap-12">
-          <span>Find us in over 20,000 stores nationwide! &nbsp;•&nbsp;</span>
-          <span>Find us in over 20,000 stores nationwide! &nbsp;•&nbsp;</span>
-          <span>Find us in over 20,000 stores nationwide! &nbsp;•&nbsp;</span>
-          <span>Find us in over 20,000 stores nationwide! &nbsp;•&nbsp;</span>
-          <span>Find us in over 20,000 stores nationwide! &nbsp;•&nbsp;</span>
+          <span>
+            100% Natural , Lab verified and Mineral water based Ice!
+            &nbsp;•&nbsp;
+          </span>
+          <span>
+            100% Natural , Lab verified and Mineral water based Ice!
+            &nbsp;•&nbsp;
+          </span>
+          <span>
+            100% Natural , Lab verified and Mineral water based Ice!
+            &nbsp;•&nbsp;
+          </span>
+          <span>
+            100% Natural , Lab verified and Mineral water based Ice!
+            &nbsp;•&nbsp;
+          </span>
+          <span>
+            100% Natural , Lab verified and Mineral water based Ice!
+            &nbsp;•&nbsp;
+          </span>
         </div>
-        <div className="flex text-white text-[40px] font-bold uppercase gap-12" aria-hidden="true">
-          <span>Find us in over 20,000 stores nationwide! &nbsp;•&nbsp;</span>
-          <span>Find us in over 20,000 stores nationwide! &nbsp;•&nbsp;</span>
-          <span>Find us in over 20,000 stores nationwide! &nbsp;•&nbsp;</span>
-          <span>Find us in over 20,000 stores nationwide! &nbsp;•&nbsp;</span>
-          <span>Find us in over 20,000 stores nationwide! &nbsp;•&nbsp;</span>
+        <div
+          className="flex text-white text-[40px] font-bold uppercase gap-12"
+          aria-hidden="true"
+        >
+          <span>
+            100% Natural , Lab verified and Mineral water based Ice!
+            &nbsp;•&nbsp;
+          </span>
+          <span>
+            100% Natural , Lab verified and Mineral water based Ice!
+            &nbsp;•&nbsp;
+          </span>
+          <span>
+            100% Natural , Lab verified and Mineral water based Ice!
+            &nbsp;•&nbsp;
+          </span>
+          <span>
+            100% Natural , Lab verified and Mineral water based Ice!
+            &nbsp;•&nbsp;
+          </span>
+          <span>
+            100% Natural , Lab verified and Mineral water based Ice!
+            &nbsp;•&nbsp;
+          </span>
         </div>
       </div>
       <style>
